1fb973ab0bb2788132fe675d0f16df7de17cd13f
张海明 <1938708066@qq.com>
2017-09-22 13:44:37 +0800

    Initial Commit

---
+++ CollectionViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewDemo
+//
+//  Created by zhm on 2017/9/22.
+//  Copyright © 2017年 xindongfangzaixian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewDemo
+//
+//  Created by zhm on 2017/9/22.
+//  Copyright © 2017年 xindongfangzaixian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
